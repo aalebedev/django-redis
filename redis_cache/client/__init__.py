0ee6ab4f35da79e5c6574de8c9f58aab4ff2fce3
Andrey Antukh <niwi@niwi.be>
2012-11-10 21:04:45 +0100

    Add experimental support for failover.

--- redis_cache/client/__init__.py
+++ redis_cache/client/__init__.py
@@ -2,5 +2,6 @@
 
 from .default import DefaultClient
 from .sharded import ShardClient
+from .experimental import SimpleFailoverClient
 
-__all__ = ['DefaultClient', 'ShardClient']
+__all__ = ['DefaultClient', 'ShardClient', 'SimpleFailoverClient']
